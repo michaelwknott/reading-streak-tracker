9ea15841822de09229e102cacc34e2067938d697
Arno Angerer <angerer.arno@gmail.com>
2023-10-01 13:18:51 +0200

    refactor: clean up initial calendar POC code, make it clear that it is an isolated experiment

--- apps/calendar/urls.py
+++ apps/calendar/urls.py
@@ -3,7 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:year>/<int:month>/", views.show_calendar, name="show_calendar"),
+    # Paths for initial POC of calendar (isolated from user model and the rest of the app)
+    path(
+        "calendar_in_isolation/", views.calendar_in_isolation, name="calendar_in_isolation"
+    ),  # redirects to calendar for current year and month
+    path(
+        "calendar_in_isolation/<int:year>/<int:month>/",
+        views.show_calendar_in_isolation,
+        name="show_calendar_in_isolation",
+    ),
     path("add_coding_day/", views.toggle_coding_day_today, name="toggle_coding_day_today"),
     path(
         "add_today_coding_day/", views.toggle_coding_day_by_form, name="toggle_coding_day_by_form"
--- apps/calendar/views.py
+++ apps/calendar/views.py
@@ -1,28 +1,32 @@
 # apps/calendar/views.py
 
+from datetime import date
+from .models import CodingDay
+from apps.calendar.utils.coding_streak_helper import CodingStreak
+
 from django.shortcuts import render, redirect
 from datetime import datetime, date
 from .models import CodingDay
 from .script import CodingStreak
 
+def calendar_in_isolation(request):
+    today = date.today()
+    return redirect("show_calendar_in_isolation", year=today.year, month=today.month)
 
-def show_calendar(request, year, month):
+
+def show_calendar_in_isolation(request, year, month):
     coding_days = CodingDay.get_coding_days(year, month)
-
-    # NEXT:Look at this part more closely and refactor
-    ### START ###
     cs = CodingStreak(year, month, coding_days)
     html_calendar = cs.create_streak_calendar_as_html()
-    ### END ###
 
-    return render(request, "calendar/calendar.html", {"html_calendar": html_calendar})
+    return render(request, "calendar/calendar_in_isolation.html", {"html_calendar": html_calendar})
 
 
 def toggle_coding_day_today(request):
     if request.method == "POST":
         today = date.today()
         toggle_coding_day(today)
-        return redirect("show_calendar", year=today.year, month=today.month)
+        return redirect("show_calendar_in_isolation", year=today.year, month=today.month)
 
 
 def toggle_coding_day_by_form(request):
@@ -31,12 +35,12 @@ def toggle_coding_day_by_form(request):
         if date_str:
             date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
             toggle_coding_day(date_obj)
-            return redirect("show_calendar", year=date_obj.year, month=date_obj.month)
+            return redirect("show_calendar_in_isolation", year=date_obj.year, month=date_obj.month)
         else:
             # No date was provided, so do nothing and redirect to today's calendar
             # This should not happen since client-side validation is in place
             today = date.today()
-            return redirect("show_calendar", year=today.year, month=today.month)
+            return redirect("show_calendar_in_isolation", year=today.year, month=today.month)
 
 
 def toggle_coding_day(target_date):
